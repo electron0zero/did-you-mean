80d0a6f9de23f7ad30346748ae764eca10dcc89f
Suraj Nath <devversitymail@gmail.com>
2018-01-20 18:13:42 +0530

    make this code less bad

--- did-you-mean/main.py
+++ did-you-mean/main.py
@@ -27,23 +27,47 @@ def main():
             flash('required')
             return render_template('index.html', form=form)
         else:
-            result = segment_and_correct(form.search_key.data)
+            result = segment_and_correct(form.search_key.data, spelling=True, segmenting=True)
             return render_template('main.html', form=form, result=result)
     elif request.method == 'GET':
         return render_template('index.html', form=form)
 
 
-def segment_and_correct(query):
-    tokens = word_tokenize(query)
-    final_text = ""
-    for token in tokens:
-        segments = segment(spell(token))
-        if len(segments) > 1:
-            for word in segments:
-                final_text = final_text + spell(word) + " "
-        else:
-            final_text = final_text + segments[0] + " "
+def segment_and_correct(payload, spelling, segmenting):
+    tokens = word_tokenize(payload)
+
+    final_tokens = []
+
+    if (segmenting and spelling):
+        segmented = segment_tokens(tokens)
+        final_tokens = spell_tokens(segmented)
+    if (segment and not spelling):
+        final_tokens = segment_tokens(tokens)
+    if (spelling and not segmenting):
+        final_tokens = spell_tokens(tokens)
+    final_text = " ".join(final_tokens)
+    # print("is it same: ", is_it_equal(payload, final_text))
     return final_text
+
+def segment_tokens(tokens):
+    segmented_tokens = []
+    for token in tokens:
+        segments = segment(token)
+        segmented_tokens.extend(segments)
+    return segmented_tokens
+
+def spell_tokens(tokens):
+    spelled_tokens = []
+    for token in tokens:
+        spelled = spell(token)
+        spelled_tokens.append(spelled)
+    return spelled_tokens
+
+def is_it_equal(first, second):
+    try:
+       return first.lower() == second.lower()
+    except AttributeError:
+       return first == second
 
 
 if __name__ == '__main__':
